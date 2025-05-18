8251c5de2922603d2f79dcba6f4a120e2368981c
SamuraiCoding <103792284+SamuraiCoding-git@users.noreply.github.com>
2025-05-18 22:41:26 +0300

    Изменение

--- bot/bot.py
+++ bot/bot.py
@@ -18,18 +18,44 @@ async def on_startup(bot: Bot, admin_ids: list[int]):
 
 
 def register_global_middlewares(dp: Dispatcher, config: Config, session_pool=None):
+    """
+    Register global middlewares for the given dispatcher.
+    Global middlewares here are the ones that are applied to all the handlers (you specify the type of update)
+
+    :param dp: The dispatcher instance.
+    :type dp: Dispatcher
+    :param config: The configuration object from the loaded configuration.
+    :param session_pool: Optional session pool object for the database using SQLAlchemy.
+    :return: None
+    """
     middleware_types = [
         ConfigMiddleware(config),
         # DatabaseMiddleware(session_pool),
     ]
+
     for middleware_type in middleware_types:
         dp.message.outer_middleware(middleware_type)
         dp.callback_query.outer_middleware(middleware_type)
 
 
 def setup_logging():
+    """
+    Set up logging configuration for the application.
+
+    This method initializes the logging configuration for the application.
+    It sets the log level to INFO and configures a basic colorized log for
+    output. The log format includes the filename, line number, log level,
+    timestamp, logger name, and log message.
+
+    Returns:
+        None
+
+    Example usage:
+        setup_logging()
+    """
     log_level = logging.INFO
     bl.basic_colorized_config(level=log_level)
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s",
@@ -39,6 +65,16 @@ def setup_logging():
 
 
 def get_storage(config):
+    """
+    Return storage based on the provided configuration.
+
+    Args:
+        config (Config): The configuration object.
+
+    Returns:
+        Storage: The storage object based on the configuration.
+
+    """
     if config.tg_bot.use_redis:
         return RedisStorage.from_url(
             config.redis.dsn(),
@@ -50,19 +86,20 @@ def get_storage(config):
 
 async def main():
     setup_logging()
+
     config = load_config(".env")
     storage = get_storage(config)
 
     bot = Bot(token=config.tg_bot.token, default=DefaultBotProperties(parse_mode="HTML"))
     dp = Dispatcher(storage=storage)
 
-    # Удаление всех установленных команд бота
     await bot.delete_my_commands(scope=None)
 
     dp.include_routers(*routers_list)
-    register_global_middlewares(dp, config)
-    await on_startup(bot, config.tg_bot.admin_ids)
 
+    register_global_middlewares(dp, config)
+
+    await on_startup(bot, config.tg_bot.admin_ids)
     await dp.start_polling(bot)
 
 
